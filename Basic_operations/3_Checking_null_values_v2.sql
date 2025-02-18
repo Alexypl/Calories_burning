d4f1d83a878c1bd5cd6cfe0765329977a8d6cd66
Alexypl <51360612+Alexypl@users.noreply.github.com>
2025-02-18 23:58:52 +0100

    Delete Basic_operations/3_Checking_null_values_v2.sql

--- Basic_operations/3_Checking_null_values_v2.sql
+++
@@ -1,6 +0,0 @@
-SELECT column_name, 
-       COUNT(*) - COUNT(*) FILTER (WHERE column_name IS NOT NULL) AS null_count
-FROM information_schema.columns
-CROSS JOIN calories
-WHERE table_name = 'calories'
-GROUP BY column_name;
