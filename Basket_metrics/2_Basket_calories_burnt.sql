5a6436ba1971d5ed92fd4e1935f1daa9543bcc87
Alexypl <51360612+Alexypl@users.noreply.github.com>
2025-02-19 23:21:42 +0100

    Create 2_Basket_calories_burnt.sql

---
+++ Basket_metrics/2_Basket_calories_burnt.sql
@@ -0,0 +1,13 @@
+WITH age_basket AS (SELECT *, CASE WHEN age >= 20 and age < 30 THEN '20-29'
+					WHEN age >= 30 and age < 40 THEN '30-39'
+					WHEN age >= 40 and age < 50 THEN '40-49'
+					WHEN age >= 50 and age < 60 THEN '50-59'
+					WHEN age >= 60 and age < 70 THEN '60-69'
+					WHEN age >= 70 and age < 80 THEN '70-79' END as basket_age
+					FROM calories)
+
+SELECT basket_age,
+ROUND(AVG(CAST(calories as numeric)),2) as average_calories
+FROM age_basket
+GROUP BY basket_age
+ORDER BY basket_age
