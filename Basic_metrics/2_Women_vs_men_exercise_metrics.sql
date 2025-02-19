09b647b89529c78d09c45621b6d6cca9389af80c
Alexypl <51360612+Alexypl@users.noreply.github.com>
2025-02-19 23:01:30 +0100

    Update 2_Women_vs_men_exercise_metrics.sql

--- Basic_metrics/2_Women_vs_men_exercise_metrics.sql
+++ Basic_metrics/2_Women_vs_men_exercise_metrics.sql
@@ -1,5 +1,6 @@
 SELECT gender, 
 ROUND(AVG(CAST(duration as numeric)), 2) as average_duration_by_gender,
-ROUND(AVG(CAST(heart_rate as numeric)), 2) as average_heart_rate_by_gender
+ROUND(AVG(CAST(heart_rate as numeric)), 2) as average_heart_rate_by_gender,
+ROUND(AVG(CAST(body_temp as numeric)), 2) as average_body_temp_by_gender
 FROM calories
 GROUP BY gender
