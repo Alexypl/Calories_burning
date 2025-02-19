5b6c43fe17281d1d205e19850b36777a35c7bbbf
Alexypl <51360612+Alexypl@users.noreply.github.com>
2025-02-19 23:16:15 +0100

    Update 0_Basket_creation.sql

--- Basket_metrics/0_Basket_creation.sql
+++ Basket_metrics/0_Basket_creation.sql
@@ -6,7 +6,7 @@ WITH age_basket AS (SELECT *, CASE WHEN age >= 20 and age < 30 THEN '20-29'
 					WHEN age >= 40 and age < 50 THEN '40-49'
 					WHEN age >= 50 and age < 60 THEN '50-59'
 					WHEN age >= 60 and age < 70 THEN '60-69'
-					WHEN age >= 70 and age < 80 THEN '70-79' END as age_basket
+					WHEN age >= 70 and age < 80 THEN '70-79' END as basket_age
 					FROM calories)
 
 SELECT * FROM age_basket
