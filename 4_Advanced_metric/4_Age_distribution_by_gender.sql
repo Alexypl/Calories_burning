c21079cf840dc58c1f4136af3bd62312bb22cc4d
Alexypl <51360612+Alexypl@users.noreply.github.com>
2025-02-26 22:58:40 +0100

    Create 4_Age_distribution_by_gender.sql

---
+++ 4_Advanced_metric/4_Age_distribution_by_gender.sql
@@ -0,0 +1,9 @@
+SELECT gender,
+MIN(age) as min_age,
+percentile_disc(0.25) WITHIN GROUP (ORDER BY age) as age_1_queartile,
+percentile_disc(0.5) WITHIN GROUP (ORDER BY age) as age_median,
+percentile_disc(0.75) WITHIN GROUP (ORDER BY age) as age_3_queartile,
+MAX(age) as max_age
+FROM calories
+GROUP BY gender
+ORDER BY gender
