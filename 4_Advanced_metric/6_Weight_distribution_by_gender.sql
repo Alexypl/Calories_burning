262b0751b2d00c0652fd09efb7bce4c09a9a5afb
Alexypl <51360612+Alexypl@users.noreply.github.com>
2025-02-26 23:03:26 +0100

    Create 6_Weight_distribution_by_gender.sql

---
+++ 4_Advanced_metric/6_Weight_distribution_by_gender.sql
@@ -0,0 +1,9 @@
+SELECT gender,
+MIN(weight) as min_weight,
+percentile_disc(0.25) WITHIN GROUP (ORDER BY weight) as weight_1_queartile,
+percentile_disc(0.5) WITHIN GROUP (ORDER BY weight) as weight_median,
+percentile_disc(0.75) WITHIN GROUP (ORDER BY weight) as weight_3_queartile,
+MAX(weight) as max_weight
+FROM calories
+GROUP BY gender
+ORDER BY gender
