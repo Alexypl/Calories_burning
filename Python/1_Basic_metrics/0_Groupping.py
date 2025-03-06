40c58f5f628323706d4be9cc5c969c926eb5a613
Alexypl <51360612+Alexypl@users.noreply.github.com>
2025-03-06 19:25:27 +0100

    main

--- Python/1_Basic_metrics/0_Groupping.py
+++ Python/1_Basic_metrics/0_Groupping.py
@@ -1,3 +1,7 @@
 import pandas as pd
 
 calories = pd.read_csv('calories.csv')
+
+metrics_by_age = calories.drop(columns = ['User_ID', 'Duration', 'Heart_Rate', 'Body_Temp', 'Calories', 'Age'])
+
+print(metrics_by_age.groupby('Gender').agg(['mean', 'std', 'min', 'max']))
